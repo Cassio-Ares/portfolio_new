df1abb255c7f9de284a1b007bc1b301cb1147ab2
Cassio-Ares <cassiotadeu.ads@gmail.com>
2024-08-07 15:37:29 -0300

    ajust

--- js/darkLigth.js
+++ js/darkLigth.js
@@ -1,5 +1,14 @@
 window.addEventListener('load', ()=>{
-    document.getElementById('toggle_btn').addEventListener('click', ()=>{
+    const btn = document.getElementById('toggle_btn')
+    const img = document.getElementById('img_toggle')
+
+    btn.addEventListener('click', ()=>{
         document.querySelector('body').classList.toggle('ligth')
+
+        if(document.body.classList.contains('ligth')){
+            img.src = './public/icons/moon.svg';
+        }else{
+            img.src = './public/icons/sun.svg';
+        }
     })
 })
